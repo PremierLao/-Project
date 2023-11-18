595b432625701c6ae88f79dbb0a0ca0d668bed8d
Lao <email@1657098590.com>
2023-11-18 16:46:26 +0800

    firstpush

---
+++ src/unauthenticated-app/register.tsx
@@ -0,0 +1,42 @@
+import React, { FormEvent } from "react";
+import { Form, Input, Button } from "antd";
+import { LongButton } from "./index";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const RegisterScreen = ({
+  onError,
+}: {
+  onError: (error: Error) => void;
+}) => {
+  const handleSubmit = () => {};
+
+  return (
+    <Form onFinish={handleSubmit}>
+      <Form.Item
+        name={"username"}
+        rules={[{ required: true, message: "请输入用户名" }]}
+      >
+        <Input placeholder={"用户名"} type="text" id={"username"} />
+      </Form.Item>
+      <Form.Item
+        name={"password"}
+        rules={[{ required: true, message: "请输入密码" }]}
+      >
+        <Input placeholder={"密码"} type="password" id={"password"} />
+      </Form.Item>
+      <Form.Item
+        name={"cpassword"}
+        rules={[{ required: true, message: "请确认密码" }]}
+      >
+        <Input placeholder={"确认密码"} type="password" id={"cpassword"} />
+      </Form.Item>
+      <Form.Item>
+        <LongButton htmlType={"submit"} type={"primary"}>
+          注册
+        </LongButton>
+        {/*此type专指antd样式*/}
+      </Form.Item>
+    </Form>
+  );
+};
